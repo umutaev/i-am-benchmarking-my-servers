68b3765420e24f1475e828383a95e04997cb1412
umutaev <M8858110177@yandex.com>
2021-04-15 20:16:27 +0500

    Added json_serialization to experiments

--- experiments.py
+++ experiments.py
@@ -1,7 +1,10 @@
 import hashlib
 import json
+import random
+import string
 from os import urandom
 from time import time
+from sys import argv
 
 
 def cycle_cipher(times: int, payload: bytes):
@@ -10,13 +13,34 @@ def cycle_cipher(times: int, payload: bytes):
     return payload
 
 
-def json_serialization(json_payload: str):
-    ...
+def serialization(json_payload: str):
+    parsed_json = json.loads(json_payload)
+    if not type(parsed_json) == dict:
+        raise TypeError("This test only works with dict jsons.")
+    return " ".join(parsed_json.keys())
 
 
 if __name__ == "__main__":
-    start = time()
-    a = cycle_cipher(1000000, bytes(urandom(256)))
-    end = time()
-    print(end - start)
-    print(a)
+    test_number = 0
+    if len(argv) >= 3 and argv[1] == '--test' and argv[2].isdigit():
+        test_number = int(argv[2])
+    elif len(argv) >= 2 and argv[1] == '--help':
+        print(f"Usage {argv[0]} --test <test number>:\n"
+              f"0 - sha256 test - 1.000.000\n"
+              f"1 - json serialization test - about 14.000.000 symbols\n")
+        exit(0)
+    if test_number == 0:
+        random_bytes = bytes(urandom(256))
+        start = time()
+        a = cycle_cipher(1000000, random_bytes)
+        end = time()
+        print("Time: ", end - start, "s", sep="")
+    elif test_number == 1:
+        json_data = json.dumps({''.join(random.choices(string.ascii_letters, k=128)): random.randint(0, 1000000)
+                                for i in range(100000)})
+        start = time()
+        a = serialization(json_data)
+        end = time()
+        print("Time: ", end - start, "s", sep="")
+    else:
+        print(f"{argv[0]} --help")
